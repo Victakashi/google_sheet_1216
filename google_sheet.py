a57c4c1c7fd8b416e5b561ab3dab7e83e9979371
Victakashi <95895366+Victakashi@users.noreply.github.com>
2021-12-16 16:40:16 +0800

    Add files via upload

--- google_sheet.py
+++ google_sheet.py
@@ -1,9 +1,18 @@
 
-# from oauth2client.service_account import ServiceAccountCredentials
+#from oauth2client.service_account import ServiceAccountCredentials
 import gspread
-# scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/driver.file', 'https://www.googleapis.com/auth/driver']
+scope = ['https://www.googlapis.com/feeds',
+         'https://www.googlapis.com/auth/spreadsheets',
+         'https://www.googleapis.com/auth/drive.file',
+         'https://www.googlapis.com/auth/drive']
 
-# creds = ServiceAccountCredentials.from_json_keyfile_name('Python Sheet API Key.json',scope)
+gc = gspread.service_account(filename= "Python Sheet API Key.json")
+
+sheet = gc.open("MZ_bot").worksheet("MZ_1")
+sheet.update_cell(1,1,"Hello world123")
+
+
+
 # client = gspread.authorize(creds)
 
 # abc = client.open("MZ_bot").worksheet("MZ_1")
@@ -11,10 +20,13 @@ import gspread
 
 
 
-sa = gspread.service_account()
-sh = sa.open("MZ_bot")
-wks = sh.worsheet("MZ_1")
+#sa = gspread.service_account()
 
-wks.update_cell(1,1, "Good")
+
+
+# sh = sa.open("MZ_bot")
+# wks = sh.worsheet("MZ_1")
+
+# wks.update_cell('A3', 'Good')
 
 
