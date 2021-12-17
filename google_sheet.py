9465aaaf53b907e25691860c85284e61f1dac237
Victakashi <95895366+Victakashi@users.noreply.github.com>
2021-12-17 12:08:14 +0800

    Update google_sheet.py

--- google_sheet.py
+++ google_sheet.py
@@ -8,7 +8,9 @@ scope = ['https://www.googlapis.com/feeds',
 
 gc = gspread.service_account(filename= "Python Sheet API Key.json")
 
-sheet = gc.open_by_key('15g-2coTEOJ5NzERQHg6N-8fKyHnUGo-g2cxX-xyTPv8').sgeet1
+#sheet = gc.open_by_key('15g-2coTEOJ5NzERQHg6N-8fKyHnUGo-g2cxX-xyTPv8').sgeet1
+sheet = gc.open('MZ_bot').sgeet1
+
 sheet.update_cell(1,1,"Hello world121701")
 
 
