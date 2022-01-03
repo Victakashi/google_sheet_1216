d0ffb5a965fcfdbbe480a93b988fb53a953400f1
Victakashi <95895366+Victakashi@users.noreply.github.com>
2022-01-03 13:49:13 +0800

    Update google_sheet1217.py

--- google_sheet1217.py
+++ google_sheet1217.py
@@ -4,10 +4,10 @@ gc = gspread.service_account(filename = "mzbot123001.json")
 
 sheet = gc.open_by_key("15g-2coTEOJ5NzERQHg6N-8fKyHnUGo-g2cxX-xyTPv8")
 worksheet = sheet.sheet1
-worksheet.update_cell(1,1, "Hello world888999993")
-worksheet.update_cell(1,2, "Hello world8889922293")
-worksheet.update_cell(1,3, "Hello world85559993")
-
+# worksheet.update_cell(1,1, "Hello world888999993")
+# worksheet.update_cell(1,2, "Hello world8889922293")
+# worksheet.update_cell(1,3, "Hello world85559993")
+worksheet.batch_clear(["A1:B1", "C2:E2", "my_named_range"])
 
 
 
